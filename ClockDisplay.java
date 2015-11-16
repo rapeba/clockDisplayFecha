ed7ab3175884152b191b89621b96e04af2843562
Your Name <raulperezbaza@gmail.com>
2015-11-16 10:49:37 +0100

    añadidos dos atributos NumberDisplay, (horas, minutos)

---
+++ ClockDisplay.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class ClockDisplay here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ClockDisplay
+{
+    // instance variables - replace the example below with your own
+    private NumberDisplay horas;
+    private NumberDisplay minutos;
+
+    /**
+     * Constructor for objects of class ClockDisplay
+     */
+    public ClockDisplay()
+    {
+        // initialise instance variables
+       
+    }
+
+  
+}
