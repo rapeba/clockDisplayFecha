f87347a3db50071d91102c1f96be9176d7bc226b
Your Name <raulperezbaza@gmail.com>
2015-11-16 11:35:52 +0100

    añadido constructor que representa las horas y minutos que queramos

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -10,21 +10,46 @@ public class ClockDisplay
     // instance variables - replace the example below with your own
     private NumberDisplay horas;
     private NumberDisplay minutos;
-    private String HoraActual;
+    private String horaActual;
 
     /**
-     * Constructor for objects of class ClockDisplay
+     * Constructor que fija la hora a "00:00" 
      */
     public ClockDisplay()
     {
         // initialise instance variables
         int horas = 00;
         int minutos = 00;
-        HoraActual = horas + ":" + minutos;
+        horaActual = "0" + horas + ":0" + minutos;
        
     }
 
     /**
-     * 
+     * Constructor que representa las horas y los minutos a los que fijar la hora actual.
      */
+    public ClockDisplay (int hours, int minutes)
+    {
+        int horas = hours;
+        int minutos = minutes;
+        
+        if (hours < 10 && minutes < 10)
+        {
+            horaActual ="0" + horas + ":0" + minutos;
+        }
+        
+        else if (hours > 10 && minutes > 10)
+        {
+            horaActual = horas + ":" + minutos;
+        }
+        
+        else if (hours < 10 && minutes > 10)
+        {
+            horaActual = "0" + horas + ":" + minutos;
+        }
+        else if (hours > 10 && minutes < 10)
+        {
+            horaActual = horas + ":0" + minutos;
+        }
+    
+}
 }
